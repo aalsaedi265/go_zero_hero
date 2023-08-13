dd629558e77c105f74d4af7fc53d9c979535f598
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-08-13 11:31:31 -0500

    better wil less code

--- practice2/main.go
+++ practice2/main.go
@@ -5,7 +5,6 @@ import "fmt"
 
 type shape interface {
 	getArea() float64
-	printArea()
 }
 
 type square struct {
@@ -21,24 +20,18 @@ func (s square) getArea() float64 {
 	return s.sideLength * s.sideLength
 }
 
-func (s square) printArea() {
-	fmt.Printf("Area of square: %f\n", s.getArea())
-}
-
 func (t triangle) getArea() float64 {
 	return 0.5 * t.base * t.height
 }
 
-func (t triangle) printArea() {
-	fmt.Printf("Area of triangle: %f\n", t.getArea())
+func printArea(s shape) {
+	fmt.Printf("Area of square: %f\n", s.getArea())
 }
 
 func main() {
 	s := square{sideLength: 4}
 	t := triangle{height: 4, base: 3}
 
-	shapes := []shape{s, t}
-	for _, sh := range shapes {
-		sh.printArea()
-	}
+	printArea(s)
+	printArea(t)
 }
