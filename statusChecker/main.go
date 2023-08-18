5cd6b99d584d860d386e4de62ac4cd841f8865fc
Ahmed Alsaedi <aalsaedi265@gmail.com>
2023-08-18 09:20:09 -0500

    done

--- statusChecker/main.go
+++ statusChecker/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 
@@ -14,17 +15,28 @@ func main() {
 		"http://golang.org",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links{
-		checkLink(link)
+		go checkLink(link, c)
+	}
+	for l := range c {
+		go func(link string){
+			time.Sleep(3* time.Second)
+			checkLink(link,c)
+		}(l)
 	}
 }
 
-func checkLink(link string){
+func checkLink(link string, c chan string){
+	time.Sleep(3*time.Second)
 	_, err := http.Get(link)
 	if err != nil{
 		fmt.Println(link, " might be down!")
+		c <- "MIght be down my shinobi"
 		return
 	}
 	fmt.Println(link, " the site is ONE ONLINE")
+	c <- link
 
 }
